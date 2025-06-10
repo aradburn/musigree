4887e6e2f50604c258eea7a1d50f9217a32daa7e
aradburn <a.radburn@btinternet.com>
2025-06-10 11:03:53 +0100

    Skip prod tests for now.

---
+++ tests/unit/app/test_fastapi_prod_app.py
@@ -0,0 +1,10 @@
+"""
+Unit tests for musigree.app.fastapi_prod_app module.
+"""
+
+import pytest
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+class TestCreateProductionApp:
+    pass
