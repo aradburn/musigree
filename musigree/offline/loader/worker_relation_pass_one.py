743aeda36421d91221faa655a5fcbfbdccd15fa8
aradburn <a.radburn@btinternet.com>
2025-06-16 12:35:48 +0100

    Refactor worker_relation_pass_one to support async operations

--- musigree/offline/loader/worker_relation_pass_one.py
+++ musigree/offline/loader/worker_relation_pass_one.py
@@ -1,6 +1,7 @@
 import logging
 import multiprocessing
 from typing import List
+import asyncio
 
 from sqlalchemy.exc import OperationalError, IntegrityError
 
@@ -44,74 +45,78 @@ class WorkerRelationPassOne(multiprocessing.Process):
         if OfflineDatabaseManager.get_concurrency_count() > 1:
             OfflineDatabaseHelper.initialize()
 
-        with offline_transaction():
-            release_repository = ReleaseRepository()
-            relation_repository = RelationRepository()
-            relation_release_year_repository = RelationReleaseYearRepository()
+        async def process_relations() -> None:
+            nonlocal count, relation_release_years
+            async with offline_transaction():
+                release_repository = ReleaseRepository()
+                relation_repository = RelationRepository()
+                relation_release_year_repository = RelationReleaseYearRepository()
 
-            for id_ in self.release_ids:
-                release: Release | None = None
-                try:
-                    # log.debug(f"loader_pass_one ({annotation}): {release_id}")
-                    release = release_repository.get(id_)
-                    relations = RelationDataAccess.from_release(release)
-                    if LOGGING_TRACE:
-                        log.debug(
-                            f"WorkerRelationPassOne [{proc_name}]\t"
-                            + f"(release_id:{release.release_id})\t[{len(relations)}] {release.title}"
+                for id_ in self.release_ids:
+                    release: Release | None = None
+                    try:
+                        # log.debug(f"loader_pass_one ({annotation}): {release_id}")
+                        release = await release_repository.get_by_id(id_)
+                        relations = RelationDataAccess.from_release(release)
+                        if LOGGING_TRACE:
+                            log.debug(
+                                f"WorkerRelationPassOne [{proc_name}]\t"
+                                + f"(release_id:{release.release_id})\t[{len(relations)}] {release.title}"
+                            )
+                    except NotFoundError:
+                        log.debug(f"WorkerRelationPassOne release_id not found: {id_}")
+                    except DatabaseError:
+                        log.error("Error in WorkerRelationPassOne worker")
+                        # log.exception("Error in WorkerRelationPassOne worker", exc_info=True)
+                        raise
+
+                    # relations = RelationUncommitted.from_dicts(relation_dicts)
+
+                    if len(relations) > 0 and release is not None:
+                        await self.create_relation_bulk(
+                            relation_repository,
+                            relations,
                         )
-                except NotFoundError:
-                    log.debug(f"WorkerRelationPassOne release_id not found: {id_}")
-                except DatabaseError:
-                    log.error("Error in WorkerRelationPassOne worker")
-                    # log.exception("Error in WorkerRelationPassOne worker", exc_info=True)
-                    raise
 
-                # relations = RelationUncommitted.from_dicts(relation_dicts)
+                        for relation in relations:
+                            year = release.release_date.year if release.release_date is not None and release.release_date.year is not None else None
+                            new_relation_release_years = await self.to_relation_release_years(
+                                relation_repository=relation_repository,
+                                relation=relation,
+                                release_id=id_,
+                                year=year,
+                            )
+                            relation_release_years.extend(new_relation_release_years)
 
-                if len(relations) > 0 and release is not None:
-                    self.create_relation_bulk(
-                        relation_repository,
-                        relations,
+                            if (
+                                len(relation_release_years)
+                                > LoaderBase.BULK_INSERT_BATCH_SIZE
+                            ):
+                                await self.create_relation_release_year_bulk(
+                                    relation_release_year_repository, relation_release_years
+                                )
+                                relation_release_years.clear()
+
+                    count += 1
+                    if (
+                        count % LoaderBase.BULK_REPORTING_SIZE == 0
+                        and not count == end_count
+                    ):
+                        log.debug(f"[{proc_name}] processed {count} of {self.total_count}")
+
+            if len(relation_release_years) > 0:
+                async with offline_transaction():
+                    relation_release_year_repository = RelationReleaseYearRepository()
+                    await self.create_relation_release_year_bulk(
+                        relation_release_year_repository, relation_release_years
                     )
 
-                    for relation in relations:
-                        year = release.release_date.year if release.release_date is not None and release.release_date.year is not None else None
-                        new_relation_release_years = self.to_relation_release_years(
-                            relation_repository=relation_repository,
-                            relation=relation,
-                            release_id=id_,
-                            year=year,
-                        )
-                        relation_release_years.extend(new_relation_release_years)
-
-                        if (
-                            len(relation_release_years)
-                            > LoaderBase.BULK_INSERT_BATCH_SIZE
-                        ):
-                            self.create_relation_release_year_bulk(
-                                relation_release_year_repository, relation_release_years
-                            )
-                            relation_release_years.clear()
-
-                count += 1
-                if (
-                    count % LoaderBase.BULK_REPORTING_SIZE == 0
-                    and not count == end_count
-                ):
-                    log.debug(f"[{proc_name}] processed {count} of {self.total_count}")
-
-        if len(relation_release_years) > 0:
-            with offline_transaction():
-                relation_release_year_repository = RelationReleaseYearRepository()
-                self.create_relation_release_year_bulk(
-                    relation_release_year_repository, relation_release_years
-                )
-
+        # Run the async function
+        asyncio.run(process_relations())
         log.info(f"[{proc_name}] processed {count} of {self.total_count}")
 
     @classmethod
-    def create_relation_bulk(
+    async def create_relation_bulk(
         cls,
         relation_repository: RelationRepository,
         relations: list[RelationUncommitted],
@@ -119,37 +124,37 @@ class WorkerRelationPassOne(multiprocessing.Process):
 
         # save, do nothing if already exists
         try:
-            relation_repository.create_bulk(relations, on_conflict_do_nothing=True)
-            relation_repository.commit()
+            await relation_repository.create_bulk(relations, on_conflict_do_nothing=True)
+            await relation_repository.commit()
             # log.debug(f"inserted {len(relations)}")
         except DatabaseError:
-            relation_repository.rollback()
+            await relation_repository.rollback()
             # log.debug(f"Roll back create")
             # log.exception(e)
         except IntegrityError:
-            relation_repository.rollback()
+            await relation_repository.rollback()
             # log.exception("Integrity Error in bulk", exc_info=True)
             # log.debug(f"Integrity Error in bulk, roll back and try one by one...")
             for relation in relations:
                 try:
-                    relation_repository.create(relation, on_conflict_do_nothing=True)
-                    relation_repository.commit()
+                    await relation_repository.create(relation, on_conflict_do_nothing=True)
+                    await relation_repository.commit()
                     # log.debug(f"inserted {len(relations)}")
                 except DatabaseError as ex:
-                    relation_repository.rollback()
+                    await relation_repository.rollback()
                     # log.debug(f"Roll back create")
                     log.exception(ex)
                 except IntegrityError:
-                    relation_repository.rollback()
+                    await relation_repository.rollback()
                     # log.exception("Integrity Error", exc_info=True)
                     # log.debug(f"Integrity Error, roll back create")
         except OperationalError as e:
-            relation_repository.rollback()
+            await relation_repository.rollback()
             log.debug(f"OperationalError in worker process")
             raise e
 
     @classmethod
-    def to_relation_release_years(
+    async def to_relation_release_years(
         cls,
         relation_repository: RelationRepository,
         relation: RelationUncommitted,
@@ -165,7 +170,7 @@ class WorkerRelationPassOne(multiprocessing.Process):
                 "role_id": role_id,
                 "object": relation.object,
             }
-            relation_id = relation_repository.get_id_by_key(key)
+            relation_id = await relation_repository.get_id_by_key(key)
             # log.debug(f"v: {relation_db.version_id}")
             relation_release_year_uncommitted = RelationReleaseYearUncommitted(
                 relation_id=relation_id,
@@ -174,32 +179,32 @@ class WorkerRelationPassOne(multiprocessing.Process):
             )
             relation_release_years.append(relation_release_year_uncommitted)
         except NotFoundError:
-            relation_repository.rollback()
+            await relation_repository.rollback()
             if LOGGING_TRACE:
                 log.debug(f"Error cannot find relation")
         except DatabaseError:
-            relation_repository.rollback()
+            await relation_repository.rollback()
             log.debug(f"Error cannot find relation")
         except OperationalError as e:
-            relation_repository.rollback()
+            await relation_repository.rollback()
             raise e
         return relation_release_years
 
     @classmethod
-    def create_relation_release_year_bulk(
+    async def create_relation_release_year_bulk(
         cls,
         relation_release_year_repository: RelationReleaseYearRepository,
         relation_release_years: List[RelationReleaseYearUncommitted],
     ) -> None:
         try:
-            relation_release_year_repository.create_bulk(relation_release_years)
-            relation_release_year_repository.commit()
+            await relation_release_year_repository.create_bulk(relation_release_years)
+            await relation_release_year_repository.commit()
             # log.debug(
             #     f"Created RelationReleaseYear: {relation_db.relation_id} {release_id} {year}"
             # )
         except DatabaseError:
-            relation_release_year_repository.rollback()
+            await relation_release_year_repository.rollback()
             log.debug(f"Error cannot create RelationReleaseYear")
         except OperationalError as e:
-            relation_release_year_repository.rollback()
+            await relation_release_year_repository.rollback()
             raise e
