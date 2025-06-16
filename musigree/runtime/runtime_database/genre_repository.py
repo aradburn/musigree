7d150ecdd779fcd3774893b150294563c7f312f4
aradburn <a.radburn@btinternet.com>
2025-06-16 11:42:34 +0100

    Refactor genre_repository to support async operations

--- musigree/runtime/runtime_database/genre_repository.py
+++ musigree/runtime/runtime_database/genre_repository.py
@@ -1,5 +1,5 @@
 import logging
-from collections.abc import Iterator
+from collections.abc import AsyncIterator
 
 from sqlalchemy import Result, select
 
@@ -17,7 +17,7 @@ class GenreRepository(RuntimeBaseRepository[GenreTable]):
     """
     Repository for managing Genre objects in the runtime database.
 
-    This class provides methods for interacting with the GenreTable
+    This class provides async methods for interacting with the GenreTable
     in the runtime database, including creating, retrieving genres by ID or
     name, and getting all genres.
 
@@ -32,23 +32,22 @@ class GenreRepository(RuntimeBaseRepository[GenreTable]):
     schema_class = GenreTable
     """The SQLAlchemy table class for runtime genres."""
 
-    def all(self) -> Iterator[Genre]:
+    async def all(self) -> AsyncIterator[Genre]:
         """
         Retrieves all genres from the runtime database.
 
         Yields:
-            Iterator[Genre]: An iterator yielding each genre.
+            AsyncIterator[Genre]: An async iterator yielding each genre.
         """
-        for instance in self._all():
-            # async for instance in self._all():
+        async for instance in self._all():
             yield Genre.model_validate(instance)
 
-    def get(self, genre_id: int) -> Genre:
+    async def get_by_id(self, id_: int) -> Genre:
         """
         Retrieves a genre by its ID.
 
         Args:
-            genre_id: The ID of the genre to retrieve.
+            id_: The ID of the genre to retrieve.
 
         Returns:
             Genre: The retrieved genre.
@@ -56,17 +55,16 @@ class GenreRepository(RuntimeBaseRepository[GenreTable]):
         Raises:
             NotFoundError: If no genre is found with the given ID.
         """
-        query = select(GenreTable).where(GenreTable.id == genre_id)
+        query = select(GenreTable).where(GenreTable.id == id_)
 
-        result: Result = self.execute(query)
-        # result: Result = await self.execute(query)
+        result: Result = await self.execute(query)
 
         if not (instance := result.scalars().one_or_none()):
             raise NotFoundError
 
         return Genre.model_validate(instance)
 
-    def get_by_name(self, name: str) -> Genre:
+    async def get_by_name(self, name: str) -> Genre:
         """
         Retrieves a genre by its name.
 
@@ -79,30 +77,23 @@ class GenreRepository(RuntimeBaseRepository[GenreTable]):
         Raises:
             NotFoundError: If no genre is found with the given name.
         """
-
         query = select(GenreTable).where(GenreTable.genre_name == name)
-
-        result: Result = self.execute(query)
-        # result: Result = await self.execute(query)
+        result: Result = await self.execute(query)
 
         if not (instance := result.scalars().one_or_none()):
             raise NotFoundError
 
-        genre = Genre.model_validate(instance)
-        """Validate the DB result into the Domain object"""
+        return Genre.model_validate(instance)
 
-        return genre
-
-    def create(self, genre: Genre) -> Genre:
+    async def create(self, genre: Genre) -> Genre:
         """
         Creates a new genre in the runtime database.
 
         Args:
-            genre: The Genre object representing the genre to create.
+            genre: The Genre object to create.
 
         Returns:
             Genre: The created genre.
         """
-        instance: GenreTable = self._save(genre.model_dump())
-        # instance: GenreTable = await self._save(schema.model_dump())
+        instance: GenreTable = await self._save(genre.model_dump())
         return Genre.model_validate(instance) 
