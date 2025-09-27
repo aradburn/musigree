97be62c331d55ffa1764ceab2ea29e8d289292f5
aradburn <a.radburn@btinternet.com>
2025-09-27 10:24:29 +0100

    Increase BULK_YIELD_SIZE from 10000 to 20000 for improved processing efficiency

--- musigree/constants.py
+++ musigree/constants.py
@@ -67,7 +67,7 @@ BULK_INSERT_BATCH_SIZE = 10000
 """The batch size for bulk insert operations."""
 BULK_REPORTING_SIZE = 10000
 """The number of records to process before reporting progress."""
-BULK_YIELD_SIZE = 10000
+BULK_YIELD_SIZE = 20000
 """The number of records to stream in a chunk from the database."""
 
 class DatabaseType(enum.Enum):
