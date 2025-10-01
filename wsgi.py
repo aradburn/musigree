25a35f72ddb50034208aec40c762c609b36f4128
aradburn <a.radburn@btinternet.com>
2025-10-01 15:27:52 +0100

    Fix wsgi startup.

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
 import uvicorn
 
-from musigree.app.fastapi_prod_app import app
-
 if __name__ == "__main__":
     # Run the application using uvicorn
     # For running behind a proxy, use the --root-path parameter as needed
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=4)
+    uvicorn.run("musigree.app.fastapi_prod_app:app", host="0.0.0.0", port=8080, workers=1, access_log=False, log_level="debug")
