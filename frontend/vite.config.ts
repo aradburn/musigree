aebaeb0d9799fd940aaf451316d00221cf5d23cc
aradburn <a.radburn@btinternet.com>
2025-11-02 22:50:53 +0000

    Fix vite config.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import removeConsole from "vite-plugin-remove-console";
 
