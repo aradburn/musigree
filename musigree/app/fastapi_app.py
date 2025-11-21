1d57e5791fcf71306ac4b1c78abfc664244a2eae
aradburn <a.radburn@btinternet.com>
2025-11-21 11:10:03 +0000

    Update CSP and CORS for ads.

--- musigree/app/fastapi_app.py
+++ musigree/app/fastapi_app.py
@@ -129,6 +129,7 @@ def create_app(config: Configuration) -> FastAPI:
         allowed_origins = [
             "https://www.musigree.com",  # Add your production domain
             "https://musigree.com",  # Add your production domain
+            "https://pagead2.googlesyndication.com",  # Adverts domain
         ]
         log.debug("Configuring CORS for production")
         log.debug(f"Allowed origins: {allowed_origins}")
