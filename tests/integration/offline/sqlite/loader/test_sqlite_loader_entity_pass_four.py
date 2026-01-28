6fe1afc8d5877404aceb1ce7c35cc899bf9b876f
aradburn <a.radburn@btinternet.com>
2026-01-28 22:21:03 +0000

    Rename tests to use offline or runtime as appropriate.

---
+++ tests/integration/offline/sqlite/loader/test_sqlite_loader_entity_pass_four.py
@@ -0,0 +1,8 @@
+from tests.integration.offline.loader.test_loader_entity_pass_four import (
+    TestLoaderEntityPassFour,
+)
+
+
+class TestSqliteLoaderEntityPassFour(TestLoaderEntityPassFour):
+    # Run all tests in TestLoaderEntityPassFour
+    pass
